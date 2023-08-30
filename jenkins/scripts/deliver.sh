ba642d843d19e4c5c02841ba64774ad02a0fc47e
AksanKolaka <aksan_usn@yahoo.com>
2023-08-30 14:54:53 +0800

    Add Deploy stage

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -21,9 +21,7 @@ set -x
 npm start &
 sleep 1
 echo $! > .pidfile
-# set +x
-set -x
-kill $(cat .pidfile)
+set +x
 
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
